ce94e26c753f13fa9061f963882f145230ff6c95
Bryan Wandrych <bdwandry@mtu.edu>
2021-06-09 19:56:21 -0400

    Added initial code

---
+++ src/PrimNumGenBigInteger.java
@@ -0,0 +1,44 @@
+import java.math.BigInteger;
+
+public class PrimNumGenBigInteger {
+    static BigInteger baseNum = new BigInteger("2");
+    static BigInteger mod2 = new BigInteger("2");
+    static BigInteger mod0 = new BigInteger("0");
+
+
+    static public boolean checkNun() throws InterruptedException {
+        BigInteger modResult = baseNum.mod(mod2);
+
+        if (modResult.equals(mod0)) {
+            return false;
+        }
+
+        BigInteger staticNumChecker = new BigInteger("2");
+        int compare = -1;
+        while (compare != 0 ) {
+            compare = staticNumChecker.compareTo(baseNum);
+
+            if (compare < 0) {
+                modResult = baseNum.mod(staticNumChecker);
+                if (modResult.equals(mod0)) {
+                    return false;
+                }
+
+                staticNumChecker = staticNumChecker.add(BigInteger.valueOf(1));
+            }
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        while (true) {
+            baseNum = baseNum.add(BigInteger.valueOf(1));
+            boolean flag = checkNun();
+
+            if (flag) {
+                System.out.println("Prime Number: " + baseNum);
+            }
+        }
+    }
+}
