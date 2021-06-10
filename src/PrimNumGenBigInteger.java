2cc15dcba4ef9a7985af7318f3667b4827818904
Bryan Wandrych <bdwandry@mtu.edu>
2021-06-09 21:10:08 -0400

    Added Save State to Desktop text file that gets written - so if the session gets killed and the file is on the desktop, it will read from it and restore from last session so it doesn't need to go back and calculate old values.

--- src/PrimNumGenBigInteger.java
+++ src/PrimNumGenBigInteger.java
@@ -2,12 +2,14 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class PrimNumGenBigInteger {
     static BigInteger baseNum = new BigInteger("2");
     static BigInteger mod2 = new BigInteger("2");
     static BigInteger mod0 = new BigInteger("0");
-
+    static BigInteger counter = new BigInteger("0");
 
     static public boolean checkNun() throws InterruptedException {
         BigInteger modResult = baseNum.mod(mod2);
@@ -35,15 +37,55 @@ public class PrimNumGenBigInteger {
     }
 
     public static void main(String[] args) throws InterruptedException, FileNotFoundException {
-        File file = new File(System.getProperty("user.home") + "/Desktop" + "/PrimeNumber.txt");
-        PrintWriter out = new PrintWriter(file);
+        File file = new File(System.getProperty("user.home") + "/Desktop" + "/PrimeNumber.txt");;
+        PrintWriter out;
+        if (file.isFile()) {
+            System.out.print("File Already Exit. Would you like to continue? <Yes> or <No>: ");
+            Scanner in = new Scanner(System.in);
+            String flag = in.nextLine();
+
+            while (!(flag.equalsIgnoreCase("Yes") || flag.equalsIgnoreCase("No"))) {
+                System.out.println("INVALID ANSWER. TYPE YES OR NO!");
+                System.out.print("Enter 'Yes' or 'No': ");
+                flag = in.nextLine();
+            }
+
+            if (flag.equalsIgnoreCase("Yes")) {
+                ArrayList<String> tempArr = new ArrayList<>();
+                Scanner lastLine = new Scanner(file);
+                while (lastLine.hasNextLine()) {
+                    String specificLine = lastLine.nextLine();
+                    tempArr.add(specificLine);
+                }
+
+                out = new PrintWriter(file);
+                for (int i = 0; i < tempArr.size(); i++) {
+                    out.println(tempArr.get(i));
+                    out.flush();
+                }
+                String[] splitString = tempArr.get(tempArr.size() - 1).split(" ");
+                counter = new BigInteger(splitString[0].substring(0, splitString.length - 4));
+                baseNum = new BigInteger(splitString[8]);
+            } else {
+                System.out.println("Restarting the Program");
+                out = new PrintWriter(file);
+            }
+
+        } else {
+            out = new PrintWriter(file);
+        }
+
         while (true) {
             baseNum = baseNum.add(BigInteger.valueOf(1));
+            long startTime = System.nanoTime();
             boolean flag = checkNun();
+            long endTime = System.nanoTime();
+            long timeElapsed = endTime - startTime;
 
             if (flag) {
-                System.out.println("Prime Number: " + baseNum);
-                out.println("Prime Number: " + baseNum);
+                counter = counter.add(BigInteger.valueOf(1));
+                System.out.println(counter + ". Time To Calculate: " + (timeElapsed/1000000 ) + " ms; Prime Number: " + baseNum);
+                out.println(counter + ". Time To Calculate: " + (timeElapsed/1000000 ) + " ms; Prime Number: " + baseNum);
                 out.flush();
             }
         }
